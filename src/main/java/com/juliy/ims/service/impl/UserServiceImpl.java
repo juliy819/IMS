e2e15ac01b87891dcba167b3637cc59df868a7b2
juliy <799399654@qq.com>
2022-09-27 16:48:56 +0800

    添加MD5加密功能，对登录密码进行加密

--- src/main/java/com/juliy/ims/service/impl/UserServiceImpl.java
+++ src/main/java/com/juliy/ims/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.juliy.ims.service.impl;
 import com.juliy.ims.dao.UserDao;
 import com.juliy.ims.dao.impl.UserDaoImpl;
 import com.juliy.ims.service.UserService;
+import com.juliy.ims.utils.Md5Util;
 
 /**
  * User业务类
@@ -14,6 +15,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean loginCheck(String username, String password) {
-        return userDao.queryUser(username, password);
+        String md5Password = Md5Util.md5Encode(password, "UTF-8");
+        return userDao.queryUser(username, md5Password);
     }
 }
